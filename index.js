dcd6005857beb40105be2f1c4f915f44c09dbffa
Olivier ANDRIKO <olivierandriko@gmail.com>
2024-07-10 20:45:10 +0200

    now sends a direct message to the discord owner in addition to the help command on join

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  ActivityType,
+  Events,
+  EmbedBuilder,
+} = require("discord.js");
 const express = require("express");
 require("dotenv").config();
 const tweetRoute = require("./router.js");
 const handleMessage = require("./events/messageEvent");
 const helpCommand = require("./commands/help");
-
 
 // Discord bot intents
 const client = new Client({
@@ -12,6 +17,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
   ],
 });
 
@@ -24,45 +30,62 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-
 client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
   client.user.setPresence({
-  activities: [{ name: `${client.guilds.cache.size} servers`, type: ActivityType.Watching }]
-});
+    activities: [
+      {
+        name: `${client.guilds.cache.size} servers`,
+        type: ActivityType.Watching,
+      },
+    ],
+  });
   await client.application.commands.create({
-    name: 'help',
+    name: "help",
     description: '❓| Useful info & links',
   });
 });
 
-  setInterval(() => {
-    client.user.setPresence({
-  	activities: [{ name: `${client.guilds.cache.size} servers`, type: ActivityType.Watching }]
-	});
-  }, 60000); 
+client.on(Events.GuildCreate, async (guild) => {
+  const channel =
+    guild.systemChannel ||
+    guild.channels.cache.find((channel) => channel.type === "GUILD_TEXT");
+  if (channel) {
+    const fakeInteraction = {
+      commandName: "help",
+      reply: async (message) => {
+        await channel.send(message);
+      },
+    };
+    helpCommand.execute(fakeInteraction);
+  }
+  const owner = await guild.fetchOwner();
+  const embed = new EmbedBuilder()
+    .setTitle(`Hello ${owner.user.username}!`)
+    .setDescription(
+      `Thanks for inviting me to your server **${guild.name}**! Consider using **/help** first and foremost.`
+    )
+    .setColor("#000000")
+    .setTimestamp();
+  await owner.send({ embeds: [embed] });
+  console.log(`Embed sent to owner ${owner.user.tag} of server ${guild.name}`);
+});
 
-  client.on('guildCreate', async (guild) => {
-    const channel = guild.systemChannel || guild.channels.cache.find(channel => channel.type === 'GUILD_TEXT');
-    if (channel) {
-      try {
-        const fakeInteraction = {
-          commandName: 'help',
-          reply: async (message) => {
-            await channel.send(message);
-          }
-        };
-        helpCommand.execute(fakeInteraction);
-      } catch (error) {
-        console.error("Could not send the help message to the new guild:", error);
-      }
-    }
+setInterval(() => {
+  client.user.setPresence({
+    activities: [
+      {
+        name: `${client.guilds.cache.size} servers`,
+        type: ActivityType.Watching,
+      },
+    ],
   });
+}, 60000);
 
-client.on('interactionCreate', async (interaction) => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === 'help') {
+  if (interaction.commandName === "help") {
     helpCommand.execute(interaction);
   }
 });
