c4c55aeb7eab557dfe55f4574bf716f5f95d6bdf
izexi <163058785+izexi7@users.noreply.github.com>
2024-07-15 13:27:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const {
-  Client,
-  GatewayIntentBits,
-  ActivityType,
-  Events,
-  EmbedBuilder,
-} = require("discord.js");
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 const express = require("express");
 require("dotenv").config();
 const tweetRoute = require("./router.js");
 const handleMessage = require("./events/messageEvent");
 const helpCommand = require("./commands/help");
+
 
 // Discord bot intents
 const client = new Client({
@@ -17,7 +12,6 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.DirectMessages,
   ],
 });
 
@@ -30,71 +24,32 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+
 client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
   client.user.setPresence({
-    activities: [
-      {
-        name: `${client.guilds.cache.size} servers`,
-        type: ActivityType.Watching,
-      },
-    ],
-  });
+  activities: [{ name: `${client.guilds.cache.size} servers`, type: ActivityType.Watching }]
+});
   await client.application.commands.create({
-    name: "help",
-    description: '❓| Useful info & links',
+    name: 'help',
+    description: 'Display basic help',
   });
 });
 
-// when joining a new discord
-client.on(Events.GuildCreate, async (guild) => {
-  const channel =
-    guild.systemChannel ||
-    guild.channels.cache.find((channel) => channel.type === "GUILD_TEXT");
-  if (channel) {
-    const fakeInteraction = {
-      commandName: "help",
-      reply: async (message) => {
-        await channel.send(message);
-      },
-    };
-    helpCommand.execute(fakeInteraction);
-  }
-  const owner = await guild.fetchOwner();
-  const embed = new EmbedBuilder()
-    .setTitle(`Hello ${owner.user.username}!`)
-    .setDescription(`Thanks for inviting me to your server **${guild.name}**!`)
-  	.addFields(
-    	{name :'Note', value :"Consider using **/help** if I don't react to your messages."})
-    .setColor("#000000")
-    .setTimestamp();
-  await owner.send({ embeds: [embed] });
-  console.log(`Message sent to owner ${owner.user.tag} of server ${guild.name}`);
-});
+  setInterval(() => {
+    client.user.setPresence({
+  	activities: [{ name: `${client.guilds.cache.size} servers`, type: ActivityType.Watching }]
+	});
+  }, 60000); 
 
-// refresh activity status every minute
-setInterval(() => {
-  client.user.setPresence({
-    activities: [
-      {
-        name: `${client.guilds.cache.size} servers`,
-        type: ActivityType.Watching,
-      },
-    ],
-  });
-}, 60000);
-
-
-// react to /help command
-client.on("interactionCreate", async (interaction) => {
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === "help") {
+  if (interaction.commandName === 'help') {
     helpCommand.execute(interaction);
   }
 });
 
-// react to twitter urls
 client.on("messageCreate", handleMessage);
 
 client.login(process.env.DISCORD_TOKEN);
