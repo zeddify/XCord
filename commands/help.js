754dab35db24c7cb65e4ff64e44e29b67d7ce6cb
Zedd <163058785+zeddify@users.noreply.github.com>
2024-08-14 19:02:14 +0100

    update(help command + change whole layout)

--- commands/help.js
+++ commands/help.js
@@ -2,15 +2,20 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   execute(interaction) {
-    const helpEmbed = new EmbedBuilder()
-      .setColor('#000000') 
-      .setTitle('How to Use')
-      .setDescription('Simply paste a Twitter URL in any channel, and it will fetch and display the tweet information and media.')
-      .addFields(
-        { name: 'Note', value: 'If the bot seems to ignore your messages, it might not have sufficient permissions in the channel. The bot needs both **READ MESSAGE HISTORY** and **SEND MESSAGES** permissions to function correctly.' },
-        { name: 'Support', value: 'If you believe the bot should be working but isn\'t, head to the [support server](https://discord.gg/5YGJvZ2fh3).' }
-      );
+    const botName = interaction.client.user.username;
+    const botAvatar = interaction.client.user.displayAvatarURL();
 
+    const helpEmbed = new EmbedBuilder()
+      .setColor('Blue')
+      .setDescription('**How do I fetch images from a Twitter/X link?**\n> Simply paste a Twitter/X URL in any channel, and it will fetch and display the posts information and media.\n\n**Why is XCord not fetching media?**\n> This will happen if XCord does not have sufficient permissions in the channel. The bot requires both `ReadMessageHistory` and `SendMessages` permissions to function correctly.') 
+      .setAuthor({ name: `${botName} Help`, iconURL: botAvatar })
+      .addFields(
+        { 
+          name: 'Important links', 
+          value: '[Support](https://discord.gg/5YGJvZ2fh3)\n[Invite](https://discord.com/oauth2/authorize?client_id=1257966131101302854)\n[Website](https://x.com/)' 
+        }
+      );
+    
     interaction.reply({ embeds: [helpEmbed] });
   }
 };
